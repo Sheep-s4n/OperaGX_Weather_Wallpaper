03c82f9daa8eb224e06b768ae0e36203d61a0e77
TheSheep_noob <yohann.vdh2008@gmail.com>
2023-08-25 17:04:30 +0200

    added time-dependent wallpapers

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 import random
 from os import remove , listdir
-from datetime import date
+import datetime
 from pynput.keyboard import Key, Controller as KeyController
 from pynput.mouse import Button, Controller as MouseController
 from screeninfo import get_monitors
@@ -29,12 +29,50 @@ monitors = get_monitors()
 keyboard = KeyController()
 mouse = MouseController()
 
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
 def getWallpaper(object , key):
     wallpaper = object[key]
+    if isinstance(wallpaper, dict):
+        current_time = datetime.time( time.localtime().tm_hour ,  time.localtime().tm_min)
+        start_hour, start_min = map(int, object[key]["start-time"].split(":"))
+        start_time = datetime.time(start_hour , start_min)
+        end_hour, end_min = map(int, object[key]["end-time"].split(":"))
+        end_time = datetime.time(end_hour , end_min)
+        if time_in_range(start_time , end_time , current_time):
+            wallpaper = object[key]["wallpaper"]
+        else: 
+            return False
     if isinstance(wallpaper , list):
-        index = random.randint(0, len(wallpaper) - 1)
-        wallpaper = wallpaper[index]
+        # check if dict
+        has_time_wallpaper=False
+        for url in wallpaper:
+           if isinstance(url , dict):
+                has_time_wallpaper = True
+                break
+        if has_time_wallpaper:
+            # generator expression
+            # {what to return} for {element name} in {list} if {condition for returning}
+            i = -1
+            for wallpp in wallpaper:
+                i+=1
+                if not isinstance(wallpp, dict): continue
+                wallpaper_res = getWallpaper(object[key] , i)
+                # wallpaper is a valid time wallpaper
+                if wallpaper_res: return wallpaper_res
+        
+        # current time is not on any wallpaper time range
+        no_time_wallpapers = [url for url in wallpaper if isinstance(url, str)]
+        index = random.randint(0, len(no_time_wallpapers) - 1)
+        wallpaper = no_time_wallpapers[index]
     return wallpaper
+
+
 
 def writeJSON(path, json) :
     with open(path, 'w') as json_file: 
@@ -100,14 +138,30 @@ def main(startup):
                 if desc in weather:
                     weather = desc 
                     break
+        
+        def switchToDefaultWallpaper():
+            print(f"No wallpaper matches for {weather} at {time.strftime('%H:%M' , time.localtime())}")
+            print("Switching to default wallpaper")
+            wallpaper_url = getWallpaper(settings["themes"] , "default")
+            if not wallpaper_url:
+                print(f"No default wallpaper found for {time.strftime('%H:%M' , time.localtime())}")
+                print("Closing function...")
+                return False
+            else:
+                return wallpaper_url
 
         if weather in settings["themes"]:
             wallpaper_url = getWallpaper(settings["themes"] , weather)
+            if not wallpaper_url:
+                wallpaper_url = switchToDefaultWallpaper()
+                # no default wallpaper
+                if not wallpaper_url:
+                    return
         else:
-            print(f"No wallpaper matches for {weather}")
-            print("Switching to default wallpaper")
-            wallpaper_url = getWallpaper(settings["themes"] , "default")
-
+            wallpaper_url = switchToDefaultWallpaper()
+            # no default wallpaper
+            if not wallpaper_url:
+                return
 
         path = settings["themes files path"]
         themes = listdir(path)
@@ -124,7 +178,7 @@ def main(startup):
                 writeJSON(DATA_FILE , 
 f'''{{
     "last_weather": "{weather}",
-    "last_updated": "{f"{date.today()}/{time.strftime('%H:%M:%S' , time.localtime())}"}",
+    "last_updated": "{f"{datetime.date.today()}/{time.strftime('%H:%M:%S' , time.localtime())}"}",
     "last_wallpaper": "{wallpaper_url}",
     "last_theme": "{new_theme_name}"
 }}''') 
@@ -176,7 +230,7 @@ f'''{{
         writeJSON(DATA_FILE , 
 f'''{{
     "last_weather": "{weather}",
-    "last_updated": "{f"{date.today()}/{time.strftime('%H:%M:%S' , time.localtime())}"}",
+    "last_updated": "{f"{datetime.date.today()}/{time.strftime('%H:%M:%S' , time.localtime())}"}",
     "last_wallpaper": "{wallpaper_url}",
     "last_theme": "{new_theme_name}"
 }}''') 
@@ -192,7 +246,7 @@ if "startup" in settings["mode"]:
     startup_param = True
 
 if "repeat" in settings["mode"]:
-    while True:
+    while "repeat" in settings["mode"]:
         main(startup_param)
         startup_param = False 
         print(f"Waiting for {settings['repeat interval']} minutes...")
